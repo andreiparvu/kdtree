300bde2d98827ebc69258e1741407ff5baea7081
Andrei Parvu <andrei.prv@gmail.com>
2014-09-20 18:38:20 +0200

    Added nearest K

--- kdtree.py
+++ kdtree.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python
 
+import Queue
+
 bestDist = 1000000
 bestNeighbour = None
+priQ, k = None, 0
 
 def dist(p1, p2):
   s = 0
@@ -30,10 +33,15 @@ class KDTree:
 
   def findNearest(self, point):
     global bestDist, bestNeighbour
+    global priQ, k
 
-    if dist(point, self.point) < bestDist:
-      bestDist = dist(point, self.point)
-      bestNeighbour = self.point
+    priQ.put((-dist(point, self.point), self.point))
+    if priQ.qsize() > k:
+      priQ.get()
+
+    # if dist(point, self.point) < bestDist:
+    #   bestDist = dist(point, self.point)
+    #   bestNeighbour = self.point
 
     other = None
     if point[self.dimIndex] < self.point[self.dimIndex]:
@@ -45,11 +53,15 @@ class KDTree:
         self.right.findNearest(point)
         other = self.right
 
-    if abs(self.point[self.dimIndex] - point[self.dimIndex]) < bestDist and other != None:
+    if (abs(self.point[self.dimIndex] - point[self.dimIndex]) < -priQ.queue[-1][0] or \
+      priQ.qsize() != k) and other != None:
       other.findNearest(point)
 
 def main():
   p = [[1, 2], [3, 4], [5, 4], [6, 3], [6, 4]]
+  global priQ, k
+  priQ = Queue.PriorityQueue()
+  k = 2
 
   kd = KDTree(p[0], 0)
   for pp in p[1:]:
@@ -57,8 +69,8 @@ def main():
 
   kd.findNearest([6, 5])
 
-  print "%s %d" % (bestNeighbour, bestDist)
-
+  # print "%s %d" % (bestNeighbour, bestDist)
+  print map(lambda x: (x[0] * -1, x[1]), priQ.queue)
 
 if __name__ == "__main__":
   main()
